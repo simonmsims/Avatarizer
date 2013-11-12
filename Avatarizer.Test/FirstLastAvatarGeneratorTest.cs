7e56e5d4715fea5206c45a4582244890b00434d5
Miha Rataj <rataj.miha@gmail.com>
2013-11-12 10:56:18 +0100

    added test project

---
+++ Avatarizer.Test/FirstLastAvatarGeneratorTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Avatarizer.Test
+{
+  using FluentAssertions;
+
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [TestClass]
+  public class FirstLastAvatarGeneratorTest
+  {
+    [TestMethod]
+    public void CreateInstanceTest()
+    {
+      var generator = new FirstLastAvatarGeneratorTest();
+      generator.Should().NotBeNull();
+    }
+  }
+}
