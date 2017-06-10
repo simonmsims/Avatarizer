6b0ec9856f4837eedb1b08045ceb3facae93a4d6
Simon Mavsar <simon.mavsar@gmail.com>
2017-06-10 16:35:54 +0200

    #297: upgrade old csproj for avatarizer project to core version

--- Avatarizer/Properties/AssemblyInfo.cs
+++ Avatarizer/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Avatarizer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Avatarizer")]
+////[assembly: AssemblyTitle("Avatarizer")]
+////[assembly: AssemblyDescription("")]
+////[assembly: AssemblyConfiguration("")]
+////[assembly: AssemblyCompany("")]
+////[assembly: AssemblyProduct("Avatarizer")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+////[assembly: AssemblyVersion("1.0.5.0")]
+////[assembly: AssemblyFileVersion("1.0.5.0")]
 
 
 [assembly: InternalsVisibleTo("Avatarizer.Test")]
